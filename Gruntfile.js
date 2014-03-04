3cd004ba3e5c6b138f38216f292613d7099d0ce6
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-03-04 09:09:16 +0800

    added grunt-contrib-watch and grunt-dot-compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,51 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    compass : {
+    compass : { // Target options
       dev : {
-        options: {              // Target options
+        options: {
           sassDir : 'public/styles',
           cssDir : 'public/styles/build',
           noLineComments : true
         }
       }
+    },
+
+    watch : {
+      styles : { // Target options
+        files : [
+          '**/*.scss' //glob file pattern
+        ],
+
+        tasks : ['compass']
+      },
+
+      templates : {
+        files : [
+          '**/*.dot'
+        ],
+
+        tasks : ['dot']
+      }
+    },
+
+    dot : {
+      dist: {
+        options: {
+          variable : 'tmpl',
+          root : __dirname + '/app',
+          requirejs : true,
+          node : true
+        },
+        src : [
+          '**/*.dot'
+        ],
+        dest : 'app/public/templates/tmpl.js'
+      }
     }
-  })
+  });
+
+  grunt.loadNpmTasks('grunt-dot-compiler');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
 };
